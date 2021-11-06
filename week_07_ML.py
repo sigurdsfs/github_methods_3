379b4290c0ef4b56adda2ebc9bbd7e93abad7d80
sigurdsfs <sigurdsfs@gmail.com>
2021-11-06 14:32:35 +0100

    message

--- week_07_ML.py
+++ week_07_ML.py
@@ -79,3 +79,21 @@ plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
 plt.show()
 
+#Perceptron run through
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1),
+ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of updates')
+plt.show()
+#%%
+msg =["nice"]
+print(msg)
+msg.append(["nice2","nice3"])
+print(msg)
+# %%
+print(msg)
+# %%
+
+# %%
